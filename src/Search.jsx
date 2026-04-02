26e0253e79051f0647430244bfd0caf1241ed5fa
Harsh Pathak <harphy45@gmail.com>
2026-04-02 23:35:20 +0530

    city chnage

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,16 +1,21 @@
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import { useState } from 'react';
 
 export default function Search(){
+    let [city,setCity] = useState("");
+    function handelCityChange(event){
+        setCity(event.target.value)
+    }
+
     return (
         <div>
             <h3>
                 Search Box
             </h3>
             <form action="">
-         <TextField id="city" label="Enter Your City" variant="outlined" required/>
+         <TextField id="city" label="Enter Your City" variant="outlined" value={city} onChange={handelCityChange} required/>
         <Button variant="contained" type='submit' endIcon={<SendIcon />}>
             Send
         </Button>
