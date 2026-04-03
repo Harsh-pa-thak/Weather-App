f8c482d9831079d5a64f361bb75a5724f1c802f7
Harsh Pathak <harphy45@gmail.com>
2026-04-03 16:36:25 +0530

    search jsx modifed

--- src/Search.jsx
+++ src/Search.jsx
@@ -8,15 +8,19 @@ export default function Search(){
     let [city,setCity] = useState("");
     function handelCityChange(event){
         setCity(event.target.value)
-        console.log(city);
     }
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log(city);
+        setCity("")
 
+    }
     return (
         <div>
             <h3>
                 Search Box
             </h3>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
          <TextField className='textField' id="city" label="Enter Your City" variant="outlined" value={city} onChange={handelCityChange} required/>
         <Button className='button' variant="contained" type='submit' endIcon={<SendIcon />}>
             Send
