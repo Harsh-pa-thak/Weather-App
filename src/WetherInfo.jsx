5989acf12434d8e0c1ea8d4d616f07cc5a9f1a41
Harsh Pathak <harphy45@gmail.com>
2026-04-12 18:53:37 +0530

    w card

--- src/WetherInfo.jsx
+++ src/WetherInfo.jsx
@@ -1,14 +1,15 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+
 import Typography from '@mui/material/Typography';
 
 
 export default function Weather(){
     let imageAdd="https://t4.ftcdn.net/jpg/05/13/26/73/360_F_513267391_QEmNGeOFLLqrILTnoq21dReUPp5UsoNr.jpg";
     let info ={
+        city:"Delhi",
         dis:"few clouds",
         humidity:64,
         temp:33.6,
@@ -19,7 +20,7 @@ export default function Weather(){
     return (
         <div className="weather-info">
             <h2>
-                Weather
+                Weather {info.dis}
             </h2>
              <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -29,17 +30,17 @@ export default function Weather(){
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {info.dis}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          <div>Temperature = {info.temp}&deg; C </div>
+          <div>Humidity = {info.humidity}</div>
+          <div>Min. Temperature = {info.tempmin}&deg; C </div>
+          <div>Max. Temperature = {info.tempMax}&deg; C </div>
+         
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      
     </Card>
         </div>
         
